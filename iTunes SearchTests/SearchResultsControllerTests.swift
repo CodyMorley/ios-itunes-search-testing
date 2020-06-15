a834e964fc87087c15ac1427f0fee17937e3f073
Cody Morley <codymorleyinc@gmail.com>
2020-06-15 10:27:21 -0700

    finished test

--- iTunes SearchTests/SearchResultsControllerTests.swift
+++ iTunes SearchTests/SearchResultsControllerTests.swift
@@ -20,16 +20,22 @@ class SearchResultsControllerTests: XCTestCase {
             e.fulfill()
         }
         wait(for: [e], timeout: 2)
-        XCTAssertTrue(controller.searchResults.count > 0, "Expecting at least one result for Garageband.")
+        XCTAssertTrue(controller.searchResults.count > 0, "Expecting at least one result for GarageBand.")
     }
     
     func testSearchResultController() {
         let mock = MockDataLoade(data: garageBandJSON, response: nil, error: nil)
-        
         let resultsExpectation = expectation(description: "Wait for search results.")
+        let controller = SearchResultController(dataLoader: mock)
         
-        let controller = SearchResultController(dataLoader: )
+        controller.performSearch(for: "GarageBand", resultType: .software) {
+            resultsExpectation.fulfill()
+        }
         
+        wait(for: [resultsExpectation], timeout: 2)
+        XCTAssertTrue(controller.searchResults == 2, "Expecting 2 results for GarageBand.")
+        XCTAssertEqual("GarageBand", controller.searchResults[0].title)
+        XCTAssertEqual("Apple", controller.searchResults[0].artist)
     }
     
 }
--- iTunes SearchTests/mockJSON.swift
+++ iTunes SearchTests/mockJSON.swift
@@ -2,8 +2,24 @@
 //  mockJSON.swift
 //  iTunes SearchTests
 //
-//  Created by Bling Morley on 6/15/20.
+//  Created by Cody Morley on 6/15/20.
 //  Copyright © 2020 Lambda School. All rights reserved.
 //
 
 import Foundation
+
+let garageBandJSON = """
+{
+    "resultCount" : 2,
+    "results": [
+        {
+            "trackName": "GarageBand",
+            "artistName": "Apple"
+        },
+        {
+            "trackName": "Shortcut: GarageBand Edition",
+            "artistName": "Mark Keroles"
+        }
+    ]
+}
+""".data(using: .utf8)
